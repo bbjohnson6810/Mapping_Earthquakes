d79d1c39e45d1f75d19dd8e443c2495b6dd07ffb
bbjohnson23 <bbj23@cornell.edu>
2022-08-18 23:57:23 -0700

    added single point to map

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -6,16 +6,28 @@ console.log("working");
 
 // alternative map object
 let myMap = L.map("map", {
-	center: [40.7, -94.5],
-	zoom: 4
+	center: [34.0522, -118.2437],
+	zoom: 14
   });
 
+// LA marker
+// L.circle([34.0522, -118.2437], {
+//   color: 'black',
+//   fillColor: 'yellow',
+//   fillOpacity: 0.5,
+//   radius: 300}).addTo(myMap);
+
+L.circleMarker([34.0522, -118.2437], {
+  color: 'black',
+  fillColor: '#ffffa1',
+//  fillOpacity: 0.5,
+  radius: 300}).addTo(myMap);
 
 //  create the tile layer to serve as map background
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -23,6 +35,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // add tile layer to the map.
 streets.addTo(myMap);
+
 
 
 console.log("still working");
