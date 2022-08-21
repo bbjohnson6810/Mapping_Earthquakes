ebb94a7a8f6838ae2ecc1d9d29bb06a34eecc1e5
bbjohnson23 <bbj23@cornell.edu>
2022-08-20 23:08:11 -0700

    simple point

---
+++ mapping_geoJSON_points/static/js/logic.js
@@ -0,0 +1,50 @@
+// check to see if the code is working.
+console.log("working");
+
+// create the map object
+// let map = L.map("mapid").setView([40.7, -94.5], 4);
+
+// alternative map object
+let myMap = L.map("map", {
+	center: [37.5, -122.5],
+	zoom: 10
+  });
+
+  // Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+L.geoJSON(sanFranAirport).addTo(myMap);
+
+//  create the tile layer to serve as map background
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// add tile layer to the map.
+streets.addTo(myMap);
+
+
+
+console.log("still working");
