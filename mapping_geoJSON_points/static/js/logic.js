ed0ae45e75245c5518a45ac6e6bf05e3cf75a21c
bbjohnson23 <bbj23@cornell.edu>
2022-08-22 09:26:22 -0700

    added multiple base maps

--- mapping_geoJSON_points/static/js/logic.js
+++ mapping_geoJSON_points/static/js/logic.js
@@ -1,52 +1,6 @@
 // check to see if the code is working.
 console.log("working");
 
-// create the map object
-// let map = L.map("mapid").setView([40.7, -94.5], 4);
-
-// alternative map object
-let myMap = L.map("map", {
-	center: [37.5, -122.5],
-	zoom: 10
-  });
-
-  // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
-
-// L.geoJSON(sanFranAirport, {
-//   // We turn each feature into a marker on the map.
-//   pointToLayer: function(feature, latlng) {
-//     console.log(feature);
-//     return L.marker(latlng)
-//     .bindPopup("<h2>" + feature.properties.city + "</h2>");
-// //   }
-
-// }).addTo(myMap);
-
-L.geoJSON(sanFranAirport, {
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h4>Airport code: " + feature.properties.faa + "<hr class='solid'>" +
-    "Airport name: " + feature.properties.name + "</h4>");
-  }
-}).addTo(myMap);
 
 //  create the tile layer to serve as map background
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -58,9 +12,44 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
+// dark tile layer
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+//  map object
+let myMap = L.map('map', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
 // add tile layer to the map.
-streets.addTo(myMap);
+// streets.addTo(myMap);
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(myMap);
 
 
+let airportData = "static/data/majorAirports.json";
 
-console.log("still working");
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+
+  L.geoJSON(data, {
+    onEachFeature: function(feature, layer) {
+  //    console.log(layer);
+      layer.bindPopup("<h4>Airport code: " + feature.properties.faa + "<hr class='solid'>" +
+      "Airport name: " + feature.properties.name + "</h4>");
+    }
+  }).addTo(myMap);});
+
